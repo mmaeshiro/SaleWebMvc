0087ec9e787f9f1c38279f34b4866e7d90610c5d
anonimous2020 <maeshiro.michael@gmail.com>
2021-01-29 10:09:31 -0300

    novo departamento

---
+++ SaleWebMvc/Controllers/DepartimentsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SaleWebMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SaleWebMvc.Controllers
+{
+    public class DepartimentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> listDepartment = new List<Department>();
+            listDepartment.Add(new Department { Id = 1, Name = "Eletronicos" });
+            listDepartment.Add(new Department { Id = 2, Name = "Fashion" });
+
+            return View(listDepartment);
+        }
+    }
+}
