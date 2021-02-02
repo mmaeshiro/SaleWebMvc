637602e444e680397bc8126d5dbd97f8d16130e2
anonimous2020 <maeshiro.michael@gmail.com>
2021-02-02 19:59:45 -0300

    other entities

---
+++ SaleWebMvc/Models/SalesRecord.cs
@@ -0,0 +1,30 @@
+﻿using SaleWebMvc.Models.Enums;
+using System;
+
+namespace SaleWebMvc.Models
+{
+    public class SalesRecord
+    {
+        public int Id { get; set; }
+        public DateTime Data { get; set; }
+        public double Amount { get; set; }
+        public SaleStatus Status { get; set; }
+
+        //Relacionamento
+        public Seller Seller { get; set; }
+
+        public SalesRecord()
+        {
+
+        }
+
+        public SalesRecord(int id, DateTime data, double amount, SaleStatus status, Seller seller)
+        {
+            Id = id;
+            Data = data;
+            Amount = amount;
+            Status = status;
+            Seller = seller;
+        }
+    }
+}
---
+++ SaleWebMvc/Models/Seller.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SaleWebMvc.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public double BaseSalary { get; set; }
+
+        //Relacionamento
+        public Department Department { get; set; }
+        //Relacionamento
+        public ICollection<SalesRecord> SalesRecords { get; set; } = new List<SalesRecord>();
+
+        public Seller() { }
+
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        public void AddSaleRecords(SalesRecord sr)
+        {
+            SalesRecords.Add(sr);
+        }
+
+        public void RemoveSaleRecords(SalesRecord sr)
+        {
+            SalesRecords.Remove(sr);
+        }
+
+        public double TotalSaleRecords(DateTime initial, DateTime final)
+        {
+            return SalesRecords.Where(x => x.Data >= initial && x.Data <= final)
+                               .Sum(x => x.Amount);
+        }
+    }
+}
